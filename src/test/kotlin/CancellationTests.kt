4ab869997a49a0eff5dd10358e786185bac43aea
James Macindoe <james.macindoe@gmail.com>
2020-04-14 15:08:45 +1000

    Add basic cancel() test

--- src/test/kotlin/CancellationTests.kt
+++ src/test/kotlin/CancellationTests.kt
@@ -4,9 +4,29 @@ import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Assert.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
 
 object CancellationTests : Spek({
     describe("Cancelling coroutines") {
+        it("cancels all children") {
+            val results = mutableListOf<Int>()
+
+            runBlocking {
+                GlobalScope.launch {
+                    launch {
+                        delay(200)
+                        results.add(1)
+                    }
+
+                    results.add(2)
+                    delay(100)
+                    cancel()
+                }.join()
+            }
+
+            assertEquals(listOf(2), results)
+        }
+
         describe("join()") {
             it("rethrows CancellationException") {
                 runBlocking {
